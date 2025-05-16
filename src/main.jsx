7e241ea57cdef0d61cdc08e5d5b1fe69836e416e
Sloptamusprime <pzyka10@gmail.com>
2025-05-16 17:03:56 -0400

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import DraftRoom from './components/DraftRoom';
 import '../index.css';
-
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <DraftRoom />
   </React.StrictMode>
 );
