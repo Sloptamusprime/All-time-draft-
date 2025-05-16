0fbeee14f38bf96e0243bb1e006fa85bb099fc28
Sloptamusprime <pzyka10@gmail.com>
2025-05-16 17:35:50 -0400

    Delete src/playerPool.js

--- src/playerPool.js
+++
@@ -1,8 +0,0 @@
-export const playerPool = [
-  { id: 1, name: 'Lionel Messi', position: 'RW', rating: 98 },
-  { id: 2, name: 'Cristiano Ronaldo', position: 'ST', rating: 97 },
-  { id: 3, name: 'Luka Modrić', position: 'CM', rating: 90 },
-  { id: 4, name: 'Virgil van Dijk', position: 'CB', rating: 89 },
-  { id: 5, name: 'Kylian Mbappé', position: 'LW', rating: 93 },
-  { id: 6, name: 'Kevin De Bruyne', position: 'CAM', rating: 91 }
-];
