582464d612fd2f3938e9aa11994a6f3b2252b1df
Sloptamusprime <pzyka10@gmail.com>
2025-05-16 22:42:30 -0400

    Update DraftRoom.jsx

--- src/components/DraftRoom.jsx
+++ src/components/DraftRoom.jsx
@@ -241,12 +241,13 @@ return (
           className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
         >
           Restart Draft
-        </button>
+               </button>
       </>
     )}
   </>
-);      
+);
 };
-          
+
+   
 export default DraftRoom;
 
