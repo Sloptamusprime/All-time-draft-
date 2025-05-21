65ce46eda97981eafe93ddde5c2b1c09d8b66142
Sloptamusprime <pzyka10@gmail.com>
2025-05-21 14:17:24 -0400

    Update DraftRoom.jsx

--- src/components/DraftRoom.jsx
+++ src/components/DraftRoom.jsx
@@ -193,10 +193,10 @@ return (
           {matchSummary && (
             <div className="mt-6 bg-gray-800 text-green-300 p-6 rounded-lg shadow whitespace-pre-wrap font-mono text-sm max-w-2xl mx-auto">
               {matchSummary}
-            </div>
-          )}
-        </>
-      )}
+              </div>
+            ))}
+        </div>
+      ))}
     </div>
   );
 };
